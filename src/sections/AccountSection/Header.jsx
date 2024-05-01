1a6c245003d50b04c543d0252a0e89e68b96f3f7
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-05-01 18:59:11 +0530

    Update Header.jsx

--- src/sections/AccountSection/Header.jsx
+++ src/sections/AccountSection/Header.jsx
@@ -1,17 +1,48 @@
-import React from 'react'
+import React from "react";
+import { IoIosArrowBack } from "react-icons/io";
+import { FcOnlineSupport } from "react-icons/fc";
 
 function Header() {
+  const goBack = () => {
+    window.history.back();
+  };
   return (
     <section>
-        <div className='flex items-center'>
+      <div className=" bg-[#212121]  mx-auto h-auto px-4 py-3 ">
+        <div className=" flex justify-between items-center">
+          <button onClick={goBack}>
+            <IoIosArrowBack className=" text-white text-2xl" />
+          </button>
 
-            <div>
-                <img src="images/logo.png" alt="" />
-            </div>
+          <div>
+            <a href="/">
+              <img src="images/logo.png" alt="" className=" h-[40px] mx-auto" />
+            </a>
+          </div>
+
+          {/* <div className=" text-white">
+            <FcOnlineSupport className=" text-2xl" />
+          </div> */}
+        </div>
+
+        <div className=" flex items-center gap-4">
+
+          <div>
+            <img src="images/avatar.png" alt=""  className=" rounded-full"/>
+          </div>
+          <div>
+          <h2>
+            MEMBERNNG
+          </h2>
+          <span>uid | 45345646</span>
+          <span>last login </span>
+
+          </div>
 
         </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Header
+export default Header;
