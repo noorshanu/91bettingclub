451224127e58784394b9df447646ed851978f6de
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-06-06 11:37:21 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,12 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+  <React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+</React.StrictMode>
   
 );
